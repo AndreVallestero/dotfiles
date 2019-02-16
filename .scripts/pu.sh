49e351f5ea533399e2ecbdc5d90cc07487bc1fe9
Andre Vallestero <andrevallestero@gmail.com>
2019-02-16 10:49:22 -0500

    Updated pu.sh to properly use reflector

--- .scripts/pu.sh
+++ .scripts/pu.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-sudo systemctl start reflector
+echo "Sorting mirrors"
+sudo reflector --threads $(nproc) -l 128 -p https -p http -p ftp --sort rate --save /etc/pacman.d/mirrorlist
+echo "Updating packages"
 yay -Syyuu
+echo "Updating dwm-status"
 kill -s 36 $(pidof dwm-status)
