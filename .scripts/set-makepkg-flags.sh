40fd211b810155967ae8690d5bbb1d52c4856b15
Andre Vallestero <andrevallestero@gmail.com>
2019-02-17 15:06:19 -0500

    Added multithreaded compression to makepkg script

--- .scripts/set-makepkg-flags.sh
+++ .scripts/set-makepkg-flags.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # TODO:
 #	- Change march=XYZ to march=native to take advantage of architecture specific opcodes
+
+if [[ $EUID != 0 ]]; then
+    echo "Script must be run with root privilages"
+    exit 1
+fi
 sed "s/$(grep "MAKEFLAGS=" /etc/makepkg.conf)/MAKEFLAGS=\"-j\$(nproc)\"/" /etc/makepkg.conf > ~/.makepkg.conf
-sed -i "s/$(grep "PKGEXT=" ~/.makepkg.conf)/PKGEXT=\'.pkg.tar\'/" ~/.makepkg.conf
+sed -i "s/$(grep "COMPRESSXZ=" ~/.makepkg.conf)/COMPRESSXZ=(xz -c -z --threads=0)/" ~/.makepkg.conf
