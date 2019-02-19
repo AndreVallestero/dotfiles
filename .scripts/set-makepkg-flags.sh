42d9547dbc6309900ed65bef834d6950f8edcd84
Andre Vallestero <andrevallestero@gmail.com>
2019-02-19 13:50:55 -0500

    Added missing dash to march

--- .scripts/set-makepkg-flags.sh
+++ .scripts/set-makepkg-flags.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed "s/-march=\S*/march=native/g; s/ -mtune=\S*//g; s/.*MAKEFLAGS=.*/MAKEFLAGS=\"-j\$(nproc)\"/; s/COMPRESSXZ=.*/COMPRESSXZ=(xz -c -z --threads=0)/" /etc/makepkg.conf > ~/.makepkg.conf
+sed "s/-march=\S*/-march=native/g; s/ -mtune=\S*//g; s/.*MAKEFLAGS=.*/MAKEFLAGS=\"-j\$(nproc)\"/; s/COMPRESSXZ=.*/COMPRESSXZ=(xz -c -z --threads=0)/" /etc/makepkg.conf > ~/.makepkg.conf
