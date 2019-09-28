e39c0cca8d6e80337a0cfce9ce6ce73a54c1a84e
Andre Vallestero <39736205+AndreVallestero@users.noreply.github.com>
2019-09-28 12:34:47 -0400

    Update set-time.sh

--- .scripts/set-time.sh
+++ .scripts/set-time.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-sudo systemctl enable ntpd
-sudo systemctl start ntpd
-sudo systemctl restart systemd-timesyncd.service
-sudo timedatectl set-ntp true
-sudo ntpd stop
+[ `id -u` -ne 0 ] && sudo $0
 sudo ntpdate ntp.ubuntu.com
-sudo ntpd start
-sudo hwclock --systohc
+sudo systemctl restart systemd-timesyncd.service
