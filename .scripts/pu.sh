07aa12f5005f95bf7e787ac3e9c41c1937c0f0f4
Andre Vallestero <39736205+AndreVallestero@users.noreply.github.com>
2019-09-28 00:13:12 -0400

    Update pu.sh

--- .scripts/pu.sh
+++ .scripts/pu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Sorting mirrors"
-sudo reflector --threads $(nproc) -l 128 -p https -p http -p ftp --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --threads $(nproc) -l 16 -p https -p http -p ftp --sort rate --save /etc/pacman.d/mirrorlist
 echo "Updating packages"
 yay -Syyuu
 echo "Updating dwm-status"
